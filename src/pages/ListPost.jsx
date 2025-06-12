7d9fbb5bfe321fd2785bfef872438e7073a73e15
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-06-12 16:38:43 +0200

    feat: creating cards

--- src/pages/ListPost.jsx
+++ src/pages/ListPost.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+
 function ListPost() {
+    const [postList, setNewList] = useState([])
+
+    const apiUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
+
+    useEffect(() => {
+
+
+        axios
+        .get(apiUrl)
+        .then((resp) => {
+            const postData = resp.data.slice(0, 11)
+            setNewList(postData)
+            console.log(postData)
+        })
+    }, [])
+
+
     return (
 
         <div className="container my-p">
             <h1>List Posts</h1>
+            <div>
+                <ul>
+                    {postList.map((curPost, index) => {
+                        <li key={index}>
+                            <div className="card">
+                                <h3>{curPost.title}</h3>
+                                <p>{curPost.body}</p>
+                            </div>
+                        </li>
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
